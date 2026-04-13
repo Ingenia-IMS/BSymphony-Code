c37925459c871286e804e990476fd13e13b81dc7
edemiguelm <e.demiguel@alumnos.upm.es>
2026-04-13 12:56:43 +0200

    na

--- firmwareC6/src/main.c
+++ firmwareC6/src/main.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+
+#define AMP_STANDBY_GPIO GPIO_NUM_20   // D9 -> !standby
+#define AUDIO_GPIO       GPIO_NUM_21   // D3 -> audio out
+
+#define TONE_FREQ_HZ     2000          // pitido de 2 kHz
+#define DUTY_RES         LEDC_TIMER_10_BIT
+#define DUTY_50_PERCENT  ((1 << 10) / 2)   // 512 con 10 bits
 
 void app_main(void)
 {
+
     while (1) {
-        printf("Hola desde el ESP32-C6\n");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
