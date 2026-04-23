ea7ef0409150a727baff0a17728897d435a63c9e
edemiguelm <e.demiguel@alumnos.upm.es>
2026-04-23 20:09:13 +0200

    no compila hay que corregir errores

--- firmwareC6/src/main.c
+++ firmwareC6/src/main.c
@@ -1,14 +1,14 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "sound_player.h"
+#include "sonido/sound_player.h"
 
 void app_main(void)
 {
     sound_player_init();
 
     while (1) {
-        sound_player_play("Tormenta");
+        sound_player_play("music");
         vTaskDelay(pdMS_TO_TICKS(7000));
     }
 }
--- firmwareC6/src/sonido/sound_catalog.c
+++ firmwareC6/src/sonido/sound_catalog.c
@@ -1,12 +1,9 @@
 #include "sound_catalog.h"
 
-#include "../sounds_h/cute_melody.h"
-#include "../sounds_h/fire_forest.h"
 #include "../sounds_h/music.h"
 
 #define SOUND_LIST \
-    X(cute_melody) \
-    X(fire_forest) \
+
     X(music)
 
 #define X(name) { #name, audio_##name, audio_##name##_len, audio_##name##_sample_rate },
